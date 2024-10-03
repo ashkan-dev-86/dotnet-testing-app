175d2d0c90fdf0d8fd07bca28b1ee1419afefd5a
ashi <ashi.dev.86@gmail.com>
2024-10-03 16:14:30 +0800

    Init primitive requirements

---
+++ DotnetTestingApp.Api/Repositories/Users/RegisterUser.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetTestingApp.Api.Repositories.Users
+{
+    internal sealed class RegisterUser
+    {
+        public sealed record request(string Email, string FirstName, string LastName, string Password);
+
+
+    }
+}
---
+++ DotnetTestingApp.Api/Repositories/Users/User.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetTestingApp.Api.Repositories.Users
+{
+    public class User
+    {
+        public Ulid Id { get; set; } = new Ulid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
